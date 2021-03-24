99ac16dbb2bed1d9127c16bb6acc6878f4c0e8f8
Roman <patlozha@gmail.com>
2021-03-25 00:37:32 +0400

    commit hello

---
+++ js/index.js
@@ -0,0 +1,21 @@
+/* describe("pow", function() {
+
+  it("возводит в степень n", function() {
+    assert.equal(pow(2, 3), 8);
+    
+  });
+
+});
+(describe("pow"));
+console.log("pow") */
+
+/* function makeUser(name, age) {
+  return {
+    name: name,
+    age: age
+    // ...другие свойства
+  };
+}
+
+let user = makeUser("Roman", 30);
+alert(user.name); */
